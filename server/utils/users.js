0fe64c6e20de919ce80bc2a2624d57f623997259
Banhegyi <212714836@logon.ds.ge.com>
2018-08-29 13:01:32 +0200

    add users class and updateuserlist event

--- server/utils/users.js
+++ server/utils/users.js
@@ -6,6 +6,39 @@ class Users{
         var user = {id,name,room};
         this.users.push(user);
     }
+
+    removeUser(id){
+        var userFound;
+        var newList = this.users.filter((user) =>{
+            if(user.id === id){
+                userFound = user; 
+            }else{
+                return user;
+            }
+           
+        });
+        
+        this.users = newList;
+        return userFound;
+    }
+
+    getUser(id){
+        var userFound = this.users.filter((user) =>{
+            return user.id === id;
+        });
+        return userFound[0];
+    }
+
+    getUserList(room){
+        var users = this.users.filter((user) =>{
+            return user.room === room;
+        });
+        var nameArray = users.map((user) =>{
+            return user.name;
+        });
+
+        return nameArray;
+    }
 }
 
 module.exports ={
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,6 +2,25 @@ const expect = require('expect');
 
 const {Users} = require('./users');
 
+var users;
+
+beforeEach(() =>{
+    users = new Users();
+    users.users = [{
+        id: 1,
+        name: 'Mike',
+        room: 'The Room'
+    },{
+        id: 2,
+        name: 'Joe',
+        room: 'The Other Room'
+    },{
+        id: 3,
+        name: 'Jen',
+        room: 'The Room'
+    }];
+});
+
 describe('Users', ()=>{
     it('should add new user', ()=>{
         var users = new Users();
@@ -10,8 +29,47 @@ describe('Users', ()=>{
             name: 'Bálint',
             room: 'The Room'
         }
-        var resultUser = users.addUser(user.id, user.name, user.room);
+        users.addUser(user.id, user.name, user.room);
         expect(users.users).toEqual([user]);
+
+    });
+
+    it('should return names of The Room users', ()=>{
+        var result = users.getUserList('The Room');
+        expect(result.length).toEqual(2);
+        expect(result).toEqual(['Mike', 'Jen']);
+
+    });
+
+    it('should return names of The Other Room users', ()=>{
+        var result = users.getUserList('The Other Room');
+        expect(result.length).toEqual(1);
+        expect(result).toEqual(['Joe']);
+
+    });
+
+    it('should remove a user', () =>{
+        var removedUser = users.removeUser(users.users[0].id);
+        expect(removedUser.name).toEqual('Mike');
+        expect(users.users.length).toEqual(2);
+        
+    });
+
+    it('should not remove a user', () =>{
+        var removedUser = users.removeUser(23);
+        expect(removedUser).toNotExist();
+    });
+
+    it('should find user', () =>{
+        var userFound = users.getUser(users.users[0].id);
+        expect(userFound.name).toEqual('Mike');
+
+    });
+
+    it('should not find user if it is not member of any room', () =>{
+        var userFound = users.getUser(23);
+        expect(userFound).toNotExist();
+
 
     });
 });
